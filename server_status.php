ffe7056a7aeeb91f2dd58eb25640890015f113b5
LouchiX <alexcachon@free.fr>
2024-10-30 20:27:31 +0100

    v0.2.11 - Correction de la fonctionnalité de vérification du statut du serveur

--- server_status.php
+++ server_status.php
@@ -4,6 +4,17 @@ header('Content-Type: application/json');
 // Exécuter le script shell et récupérer uniquement la réponse du echo
 $status = exec("sh /home/sfserver/script/startup.sh 2>&1", $output, $return_var);
 
+// Vérifiez si le script a échoué
+if ($return_var !== 0) {
+    // En cas d'erreur, retourner le message d'erreur
+    echo json_encode([
+        'status' => 'Erreur lors de l\'exécution du script',
+        'error' => implode("\n", $output), // Retourner les messages d'erreur
+        'return_var' => $return_var
+    ]);
+    exit;
+}
+
 // Retourner uniquement le statut
 echo json_encode([
     'status' => trim($status), // On utilise trim pour enlever les espaces inutiles
