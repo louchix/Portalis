5c1fac1686c20a45cc67c2f893ac488a4ea1c956
LouchiX <alexcachon@free.fr>
2024-11-03 11:07:34 +0100

    v0.5.8 - Fix Upload Blueprint

--- controlServer.php
+++ controlServer.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
-    $uploadDir = '/home/sfserver/.config/Epic/FactoryGame/Saved/SaveGames/blueprints/uWu\ Factory/';
+    $uploadDir = '/home/sfserver/.config/Epic/FactoryGame/Saved/SaveGames/blueprints/uWu Factory/';
     $fileName = basename($_FILES['file']['name']);
     $fileExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
     $allowedExtensions = ['sbp', 'sbpcfg'];
@@ -11,13 +11,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['file'])) {
         exit;
     }
 
-    $uploadFile = $uploadDir . $fileName;
+    $tempFile = "/tmp/$fileName";
 
-    // Utiliser sudo pour déplacer le fichier avec les droits de sfserver
-    if (move_uploaded_file($_FILES['file']['tmp_name'], "/tmp/$fileName")) {
-        $command = escapeshellcmd("sudo -u sfserver mv /tmp/$fileName $uploadFile");
-        shell_exec($command);
-        echo "Fichier uploadé avec succès.";
+    // Déplacer le fichier vers un emplacement temporaire
+    if (move_uploaded_file($_FILES['file']['tmp_name'], $tempFile)) {
+        // Utiliser sudo pour déplacer le fichier avec les droits de sfserver
+        $command = escapeshellcmd("sudo -u sfserver mv $tempFile '$uploadDir$fileName'");
+        $output = shell_exec($command);
+        if ($output === null) {
+            echo "Erreur lors du déplacement du fichier.";
+        } else {
+            echo "Fichier uploadé avec succès.";
+        }
     } else {
         echo "Erreur lors de l'upload.";
     }
