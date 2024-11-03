79fbe45285d900ac30484d89ce2a52868f45b19e
LouchiX <alexcachon@free.fr>
2024-11-03 10:59:21 +0100

    v0.5.5 - Control Server & Upload Blueprint & Fix Path & Fix PHP & Fix HTML & Fix CSS & Fix JS

--- controlServer.php
+++ controlServer.php
@@ -14,7 +14,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['action'])) {
     $action = $_GET['action'];
     if (in_array($action, ['start', 'stop', 'restart'])) {
         // Remplacez par la commande réelle pour contrôler le serveur
-        shell_exec("./lgsm.sh $action");
+        shell_exec("cd /home/sfserver && ./sfserver $action");
         echo "Serveur $action avec succès.";
     }
 }
