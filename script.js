59df7a15144bcf3be14e20616ebf6d3b0a83e86f
LouchiX <alexcachon@free.fr>
2024-11-03 11:10:48 +0100

    v0.5.10 - Fix Upload Blueprint & Fix JS

--- script.js
+++ script.js
@@ -3,27 +3,38 @@ function uploadBlueprint() {
     const file = fileInput.files[0];
     const allowedExtensions = ['.sbp', '.sbpcfg'];
 
+    console.log('Début du processus de téléchargement.');
+
     if (!file) {
         alert('Veuillez sélectionner un fichier.');
+        console.log('Aucun fichier sélectionné.');
         return;
     }
 
     const fileExtension = file.name.slice(file.name.lastIndexOf('.')).toLowerCase();
+    console.log(`Fichier sélectionné: ${file.name}, Extension: ${fileExtension}`);
+
     if (!allowedExtensions.includes(fileExtension)) {
         alert('Seuls les fichiers .sbp et .sbpcfg sont autorisés.');
+        console.log('Extension de fichier non autorisée.');
         return;
     }
 
     const formData = new FormData();
     formData.append('file', file);
+    console.log('FormData créé avec le fichier.');
 
     fetch('/upload', {
         method: 'POST',
         body: formData
     })
-    .then(response => response.text())
+    .then(response => {
+        console.log('Réponse reçue du serveur.');
+        return response.text();
+    })
     .then(data => {
-        alert('Blueprint uploadé avec succès');
+        console.log('Réponse du serveur traitée.');
+        alert(data);
     })
     .catch(error => {
         console.error('Erreur:', error);
