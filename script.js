8812087d31e1ee5c2ccaaee26963b30fe3de73f9
LouchiX <louchix@live.fr>
2024-11-06 13:23:47 +0100

    v1.2.20 - Ajout de la fonctionnalité de téléchargement de blueprint

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM entièrement chargé et analysé.');
-    
+
     document.getElementById('fileInput').addEventListener('change', function() {
         const fileNameSpan = document.querySelector('.file-name');
         const file = this.files[0];
@@ -13,10 +13,8 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 
-    // Vérifier l'état du serveur dès que la page est chargée
-    checkServerStatus();
-
-    listSaves();
+    // Charger les sauvegardes dès que la page est chargée
+    loadSaves();
 });
 
 function uploadBlueprint() {
@@ -61,76 +59,14 @@ function uploadBlueprint() {
         console.log('Logs du serveur :\n' + data);
         alert(data.split('\n')[0]); // Affiche uniquement le premier message à l'utilisateur
         // Recharger la liste des sauvegardes après un upload réussi
-        listSaves();
+        loadSaves();
     })
     .catch(error => {
         console.error('Erreur:', error);
     });
 }
 
-function controlServer(action) {
-    console.log(`Action du serveur: ${action}`);
-    const loadingText = document.querySelector('.loading-text'); // Sélectionnez le texte
-
-    // Ajoutez la classe de chargement pour l'animation
-    loadingText.classList.add('loading');
-
-    fetch(`controlServer.php?action=${action}`)
-    .then(response => {
-        console.log(`Réponse reçue du serveur pour l'action ${action}.`);
-        if (!response.ok) {
-            throw new Error('Erreur réseau : ' + response.statusText);
-        }
-        return response.text();
-    })
-    .then(data => {
-        console.log('Réponse du serveur traitée.');
-        console.log('Logs du serveur :\n' + data);
-        alert(data.split('\n')[0]); // Affiche uniquement le premier message à l'utilisateur
-        // Recharger la liste des sauvegardes après une action de serveur
-        listSaves();
-    })
-    .catch(error => {
-        console.error('Erreur:', error);
-    })
-    .finally(() => {
-        // Retirez la classe de chargement après la réponse
-        loadingText.classList.remove('loading');
-    });
-}
-
-function checkServerStatus() {
-    console.log('Vérification de l\'état du serveur...');
-    fetch('controlServer.php?action=status')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Erreur réseau : ' + response.statusText);
-            }
-            return response.json(); // Traiter la réponse comme JSON
-        })
-        .then(data => {
-            const statusElement = document.getElementById('serverStatus');
-            // Réinitialiser les classes avant d'ajouter la nouvelle classe
-            statusElement.classList.remove('is-info', 'is-success', 'is-danger');
-
-            if (data.status === 'ON') {
-                statusElement.classList.add('is-success'); // Changer en vert
-                statusElement.textContent = 'État du serveur : Serveur ON';
-            } else if (data.status === 'OFF') {
-                statusElement.classList.add('is-danger'); // Changer en rouge
-                statusElement.textContent = 'État du serveur : Serveur OFF';
-            } else {
-                statusElement.classList.add('is-info'); // État inconnu
-                statusElement.textContent = `État du serveur : ${data.message}`;
-            }
-            console.log(`État du serveur récupéré : ${data.status}`);
-        })
-        .catch(error => {
-            console.error('Erreur lors de la vérification de l\'état du serveur:', error);
-        });
-}
-
-function listSaves() {
+function loadSaves() {
     console.log('Récupération de la liste des sauvegardes...');
     fetch('controlServer.php?action=list_saves')
         .then(response => {
@@ -165,4 +101,4 @@ function listSaves() {
 }
 
 // Vérifier l'état du serveur toutes les 10 secondes
-setInterval(checkServerStatus, 10000);
+setInterval(loadSaves, 10000);
